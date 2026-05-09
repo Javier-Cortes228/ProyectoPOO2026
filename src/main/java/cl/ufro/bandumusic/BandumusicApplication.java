ebe08cea7d2561c4953b58cb76a81724ee67609a
Javier-Cortes228 <j.cortes10@ufromail.cl>
2026-05-09 12:50:45 -0400

    Setup inicial listo

---
+++ src/main/java/cl/ufro/bandumusic/BandumusicApplication.java
@@ -0,0 +1,13 @@
+package cl.ufro.bandumusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandumusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandumusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ufro/bandumusic/BandumusicApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ufro.bandumusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandumusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
